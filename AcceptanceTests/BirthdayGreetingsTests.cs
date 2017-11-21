5268e609e059044a59dc13adba6dfd6191bb3c10
nikitamaruniak <nikita.marunyak@gmail.com>
2017-11-21 16:08:19 +0200

    First outside-in commit.

---
+++ AcceptanceTests/BirthdayGreetingsTests.cs
@@ -0,0 +1,58 @@
+﻿using nDumbster.Smtp;
+using System;
+using System.Linq;
+using System.IO;
+using Xunit;
+using System.Diagnostics;
+using System.Net.Mail;
+
+namespace AcceptanceTests
+{
+    public class BirthdayGreetingsTests
+    {
+        [Fact]
+        public void GivenBirthdayTodaySendsEmail()
+        {
+            using (SimpleSmtpServer smtp = SimpleSmtpServer.Start(8025))
+            {
+                using (FileStream file = File.Open("employees.txt", FileMode.Create))
+                using (StreamWriter writer = new StreamWriter(file))
+                {
+                    writer.WriteLine("last_name, first_name, date_of_birth, email");
+                    writer.WriteLine(
+                        "{0}, {1}, {2}, {3}",
+                        "Doe", "John",
+                        DateTime.Now.ToString(@"yyyy\/MM\/dd"),
+                        "john.doe@foobar.com");
+                }
+
+                Process service = new Process();
+                service.StartInfo.Arguments = "127.0.0.1 8025 employees.txt";
+#if DEBUG
+                service.StartInfo.FileName = @"..\..\..\BirthdayGreetings\bin\Debug\BirthdayGreetings.exe";
+#else
+                service.StartInfo.FileName = @"..\..\..\BirthdayGreetings\bin\Release\BirthdayGreetings.exe";
+#endif
+                service.StartInfo.WorkingDirectory = Environment.CurrentDirectory;
+
+                try
+                {
+                    service.Start();
+                    service.WaitForExit();
+                }
+                finally
+                {
+                    File.Delete("employees.txt");
+                }
+
+                Assert.Equal(0, service.ExitCode);
+                Assert.Equal(1, smtp.ReceivedEmailCount);
+                MailMessage message = smtp.ReceivedEmail.First();
+                Assert.Single(message.To);
+                Assert.Equal("john.doe@foobar.com", message.To.First().Address);
+                Assert.Equal("Happy birthday!", message.Subject);
+                Assert.Equal("Happy birthday, dear John!", message.Body);
+            }
+        }
+    }
+}
---
+++ BirthdayGreetings/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+using System.Net.Mail;
+
+namespace BirthdayGreetings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string hostname = args[0];
+            int port = int.Parse(args[1]);
+            string dataPath = args[2];
+
+            using (FileStream file = File.Open(dataPath, FileMode.Open))
+            using (StreamReader reader = new StreamReader(file))
+            {
+                reader.ReadLine();
+                while (!reader.EndOfStream)
+                {
+                    string line = reader.ReadLine();
+                    string[] tokens = line.Split(
+                        new string[] { ", " },
+                        StringSplitOptions.None);
+                    string firstName = tokens[1];                    
+                    string email = tokens[3];
+                    using (SmtpClient smtpClient = new SmtpClient(hostname, port))
+                    {
+                        var subject = "Happy birthday!";
+                        var body = string.Format("Happy birthday, dear {0}!", firstName);
+                        var message = new MailMessage("noreply@noname", email, subject, body);
+                        smtpClient.Send(message);
+                    }
+                }
+            }
+        }
+    }
+}
